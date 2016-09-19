94d694e32bdde11a5f76904816e214e15b343ef9
brian <brian@falcon.local>
2016-09-19 15:01:55 -0700

    Simple code to toggle "Hello World!" message in app

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func toggleMessageLabel(_ sender: UIButton)
+    {
+        if (messageLabel.text == "")
+        {
+            messageLabel.text = "Hello World!"
+        }
+        else
+        {
+            messageLabel.text = ""
+        }
+    }
 
 }
 
